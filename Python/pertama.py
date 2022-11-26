f9320c7edd03711cb4dd132a0d625a295efcf569
sidrafhardiyanto <sidrafhardiyanto@gmail.com>
2022-11-26 14:29:42 +0700

    gataw coy

---
+++ Python/pertama.py
@@ -0,0 +1,7 @@
+print("Sidra Febrian Hardiyanto, 221240001231, Teknik Informatika UNISNU Jepara\n\n\n")
+
+print("Ini latihan index. Saya akan membuat variabel namaSaya lalu mengisinya dengan string, lalu saya akan memanggil namaSaya dengan beberapa command yang berbeda\nnamaSaya='sIdRA feBRian hArdIYANTO'")
+namaSaya="sIdRA feBRian hArdIYANTO"
+print(namaSaya[:5])
+print(namaSaya.lower()[6:13])
+print(namaSaya.upper()[14:])
