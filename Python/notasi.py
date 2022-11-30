901891ba3e542d2ce47f994302ae5ed76e91de75
sidrafhardiyanto <sidrafhardiyanto@gmail.com>
2022-11-30 21:27:57 +0700

    rapihin

--- Python/notasi.py
+++ Python/notasi.py
@@ -1,30 +1,28 @@
 print("Ini adalah program menghitung bilangan baris dan deret aritmatika dan geometri")
-print("wk"*10)
-kata=" "
-print(kata.center(60,"="))
 print("""Program ini dapat menghitung:
-1. Notasi Aritmatika (+)
-2. Notasi Geometri (x)
+1. Bilangan baris dan deret Aritmatika (+)
+2. Bilangan baris dan deret Geometri (x)
 
 Rumus
 1. Aritmatika
-    Un=a+(n-1)b
-    Sn=n/2(2*a+(n-1)b)
+    UnAritmatika=a+(n-1)*b
+    SnAritmatika=n/2*(2*a+(n-1)*b)
 2. Geometri
-    UnGeometri=a*(r**n-1)
-    SnGeometri=(a*(r**n)-1)/(r-1)
-""")
+    UnGeometri=a*(r**(n-1))
+    SnGeometri=(a*(r**n)-1)/(r-1)""")
 pilihan=int(input("Masukkan angka 1 atau 2 untuk memilih rumus yang akan dihitung : "))
 if pilihan==1:
-    a=int(input("Masukkan a : "))
+    print(" - Aritmatika - ".center(60,"A"))
+    a=int(input("\nMasukkan a : "))
     b=int(input("Masukkan b : "))
     n=int(input("Masukkan n : "))
     UnAritmatika=a+(n-1)*b
     SnAritmatika=n/2*(2*a+(n-1)*b)
-    print("Nilai Un = ",int(UnAritmatika))
-    print("Nilai Sn = ",int(SnAritmatika))
+    print("Nilai Un = ",UnAritmatika)
+    print("Nilai Sn = ",SnAritmatika)
 elif pilihan==2:
-    a=int(input("Masukkan a : "))
+    print(" - Geometri - ".center(60,"G"))
+    a=int(input("\nMasukkan a : "))
     r=int(input("Masukkan r : "))
     n=int(input("Masukkan n : "))
     UnGeometri=a*(r**(n-1))
