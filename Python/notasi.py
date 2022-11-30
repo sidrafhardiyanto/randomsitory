c0be9799409b8be8ca9cdb79fd69d08520c8f763
sidrafhardiyanto <sidrafhardiyanto@gmail.com>
2022-11-30 11:44:47 +0700

    benerin rumus yang geometri, dah bener.

--- Python/notasi.py
+++ Python/notasi.py
@@ -17,17 +17,19 @@ Rumus
 pilihan=int(input("Masukkan angka 1 atau 2 untuk memilih rumus yang akan dihitung : "))
 if pilihan==1:
     a=int(input("Masukkan a : "))
-    n=int(input("Masukkan n : "))
     b=int(input("Masukkan b : "))
+    n=int(input("Masukkan n : "))
     UnAritmatika=a+(n-1)*b
-    SnAritmatika=(n/2)*(2*a+(n-1)*b)
+    SnAritmatika=n/2*(2*a+(n-1)*b)
     print("Nilai Un = ",int(UnAritmatika))
     print("Nilai Sn = ",int(SnAritmatika))
 elif pilihan==2:
     a=int(input("Masukkan a : "))
     r=int(input("Masukkan r : "))
     n=int(input("Masukkan n : "))
-    UnGeometri=a*(r**n-1)
+    UnGeometri=a*(r**(n-1))
     SnGeometri=(a*(r**n)-1)/(r-1)
     print("Nilai Un = ",UnGeometri)
     print("Nilai Sn = ",SnGeometri)
+else:
+    print("Input error.")
